ac88033b161e79dea939087fbff9bec8908ffe6a
plug333 <120030894+plug333@users.noreply.github.com>
2023-05-04 19:47:29 +0200

    table

--- mmp-fe/src/App.tsx
+++ mmp-fe/src/App.tsx
@@ -1,5 +1,6 @@
 import { useAccount, useConnect, useBalance, usePrepareContractWrite, useContractWrite } from "wagmi";
 import { contractAbi } from "./constant/contract-abi";
+import './App.css';
 
 const contractAddress = "0xD4Be9810D708d7a12b209F0c41dB51b303622aEa";
 
@@ -26,8 +27,27 @@ function App() {
 
   console.log(data, 1);
 
+  const ncol = 4;
+  const nrig = 4;
+
+  let tabella = ""
+  for(let i = 1; i < ncol; i++) {
+    tabella = tabella + "<tr>"
+    for(let y = 1; y < nrig; y++){
+      tabella = tabella + "<td>" + i + " " + y + "</td>";
+    }
+    tabella = tabella + "</tr>"
+  }
+
   return (
     <div className="App">
+      <div id="table-container">
+        <table>
+          <tr>
+            {tabella}
+          </tr>
+        </table>
+      </div>
       {connectors.map((connector) => (
         <button key={connector.id} onClick={() => connect({ connector })}>
           {connector.name}
