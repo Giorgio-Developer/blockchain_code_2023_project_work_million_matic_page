c328ae64b010cc8a228d7b1a7a13b5c238f05989
Giorgio Sanfilippo <gaswebmaster@gmail.com>
2023-05-19 16:10:12 +0200

    Minor cleanup - Removed unused function

--- mmp-fe/src/button/connect-wallet-button.tsx
+++ mmp-fe/src/button/connect-wallet-button.tsx
@@ -4,18 +4,6 @@ import { useConnect } from "wagmi";
 export function ConnectWallet(props: any) {
 
 	const { connect, connectors } = useConnect();
-	
-	const connectWallet = async () => {
-		console.log('ConnectWallet');
-		//props.clickConnectWalletButton();
-
-		connectors.map((connector) => (
-			<button key={connector.id} onClick={() => connect({ connector })}>
-				{connector.name}
-			</button>
-		))
-
-	};
 
 	return (
 		<div className="mint-button-container">
