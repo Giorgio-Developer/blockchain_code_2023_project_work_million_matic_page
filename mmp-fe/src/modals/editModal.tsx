517085313a3902a76c60b71aa3455bde1c192025
Giorgio Sanfilippo <gaswebmaster@gmail.com>
2023-05-21 17:12:54 +0200

    Salvataggio dati su IPFS, ottenimento URI e salvataggio su blockchain adesso funziona utilizando un unico tasto [Save Changes]

--- mmp-fe/src/modals/editModal.tsx
+++ mmp-fe/src/modals/editModal.tsx
@@ -17,164 +17,188 @@ const abi = contractAbi;
 
 export function EditModal(props: any) {
 
-  interface TypeDataToSend {
-    imgURL: string;
-    altText: string;
-    webURL: string;
-    tokenId: number;
-  }
+interface TypeDataToSend {
+	imgURL: string;
+	altText: string;
+	webURL: string;
+	tokenId: number;
+}
 
-  const imageStyle = {
-    width: '100%',
-    padding: '20px',
-    margin: '0px',
-    border: '1px solid black',
-  };
+const imageStyle = {
+	width: '100%',
+	padding: '20px',
+	margin: '0px',
+	border: '1px solid black',
+};
 
-  const [imgURL, setImgURL] = useState('');
-  const [altText, setAltText] = useState('');
-  const [webURL, setWebURL] = useState('');
-  const [tokenId, setTokenId] = useState("");
-  const [contentIdentificator, setContentIdentificator] = useState('');
+const [imgURL, setImgURL] = useState('');
+const [altText, setAltText] = useState('');
+const [webURL, setWebURL] = useState('');
+const [tokenId, setTokenId] = useState("");
+const [contentIdentificator, setContentIdentificator] = useState('');
+const handleClose = () => {props.clickCloseButton()} ; // () => setShow(false);
 
-  let ipfsHash:string;
+let txExecuting = false;
 
-  const handleClose = () => {props.clickCloseButton()} ; // () => setShow(false);
+const { config } = usePrepareContractWrite({
+	address: contractAddress,
+	abi: abi,
+	functionName: "setTokenURI",
+	args: [tokenId, contentIdentificator]
+});
 
-  const { config } = usePrepareContractWrite({
-    address: contractAddress,
-    abi: abi,
-    functionName: "setTokenURI",
-    args: [tokenId, contentIdentificator]
-  });
+const { data, isLoading, isSuccess, isError, write } = useContractWrite(config);
+
+const handleTransactionStart = () => {
+		txExecuting = true;
+		console.log('Salvataggio dati su Blockchain in corso...');
+
+	}
+
+	useEffect(() => {
+		if (isLoading) {
+			handleTransactionStart();
+		}
+	}, [handleTransactionStart, isLoading]);
+
+	useEffect(() => {
+		if (props.show === true) {
+		setTokenId(props.tokenId);
+		console.log(props.tokenId);
+		}
+	}, [props.show]);
+
+	useEffect(() => {
+		if (contentIdentificator !== undefined && contentIdentificator !== '' && !txExecuting) {
+		//writeToBlockchain();
+		console.log('writing on Blockchain');
+		if (!write) return;
+		console.log(config)
+		write(); // write to blockchain
+
+		}
+	}, [contentIdentificator]);
+
+	const handleTransactionSuccess = () => {
+		txExecuting = false;
+		console.log('Dati salvati correttamente su Blockchain');
+	}
+
+	useEffect(() => {
+		if (isSuccess && data) {
+			//console.log("Transaction success", data);
+			handleTransactionSuccess();
+		}
+	}, [handleTransactionSuccess, isSuccess]);
 
 
-  useEffect(() => {
-    if (props.show === true) {
-      setTokenId(props.tokenId);
-      console.log(props.tokenId);
-    }
-  }, [props.show]);
+	const sendDataToIPFS = async () => {
 
-  const { data, write } = useContractWrite(config);
+		const dataToSend:TypeDataToSend = {
+		imgURL: imgURL,
+		altText: altText,
+		webURL: webURL,
+		tokenId: props.tokenId,
+		}
 
-  const sendDataToIPFS = async () => {
+		var dataIPFS = JSON.stringify({
+			pinataOptions: {
+				cidVersion: 1,
+			},
+			pinataMetadata: {
+				name: "NFTData",
+				keyvalues: {
+				customKey: "customValue",
+				customKey2: "customValue2",
+				},
+			},
+			pinataContent: dataToSend,
+		});
 
-    const dataToSend:TypeDataToSend = {
-      imgURL: imgURL,
-      altText: altText,
-      webURL: webURL,
-      tokenId: props.tokenId,
-    }
+		var configIPFS = {
+			method: "post",
+			url: "https://api.pinata.cloud/pinning/pinJSONToIPFS",
+			headers: {
+				pinata_api_key: PINATA_APIKEY,
+				pinata_secret_api_key: PINATA_SECRET,
+				"Content-Type": "application/json",
+			},
+			data: dataIPFS,
+		};
 
-    console.log("I dati inviati ad IPFS sono " + JSON.stringify({dataToSend}));
+		const res = await axios(configIPFS)
+			.then(function (response: any) {
+				// console.log('ipfsHash: ', response.data.IpfsHash);
+				setContentIdentificator(response.data.IpfsHash);
+				console.log('contentIdentificator: ', contentIdentificator);
+			})
+			.catch(function (error: any) {
+				// handle error
+				console.log(error);
+			});
+	};
 
-    var dataIPFS = JSON.stringify({
-      pinataOptions: {
-        cidVersion: 1,
-      },
-      pinataMetadata: {
-        name: "NFTData",
-        keyvalues: {
-          customKey: "customValue",
-          customKey2: "customValue2",
-        },
-      },
-      pinataContent: dataToSend,
-    });
 
-    var configIPFS = {
-      method: "post",
-      url: "https://api.pinata.cloud/pinning/pinJSONToIPFS",
-      headers: {
-        pinata_api_key: PINATA_APIKEY,
-        pinata_secret_api_key: PINATA_SECRET,
-        "Content-Type": "application/json",
-      },
-      data: dataIPFS,
-    };
-
-    const res = await axios(configIPFS);
-    console.log(res.data.IpfsHash); 
-
-    return res.data.IpfsHash;
-
-  };
-
-  const writeToBlockchain = async () => {
-
-    console.log('contentIdentificator: ', contentIdentificator);
-    console.log('ipfsHash: ', ipfsHash);
-
-    if (!write) return;
-    console.log(config)
-    write(); 
-
-  };
 
   function handleChange(event:any) {
-    let targetId = event.currentTarget.id;
+	let targetId = event.currentTarget.id;
 
-    switch(targetId) {
-      case 'imageUrlForm':
-        setImgURL(event.target.value);
-      break;
-      case 'altTextForm':
-        setAltText(event.target.value);
-      break;
-      case 'webURLForm':
-        setWebURL(event.target.value);
-      break;
-    }
+	switch(targetId) {
+		case 'imageUrlForm':
+			setImgURL(event.target.value);
+		break;
+		case 'altTextForm':
+			setAltText(event.target.value);
+		break;
+		case 'webURLForm':
+			setWebURL(event.target.value);
+		break;
+		}
 
   }
 
   return (
-    <div
-      className="modal show"
-      style={{ display: 'block', position: 'initial' }}
-    >
+		<div
+		className="modal show"
+		style={{ display: 'block', position: 'initial' }}
+		>
 
-      <Modal show={props.show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Edit Modal</Modal.Title>
-        </Modal.Header>
+		<Modal show={props.show} onHide={handleClose}>
+			<Modal.Header closeButton>
+			<Modal.Title>Edit Modal</Modal.Title>
+			</Modal.Header>
 
-        <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3" controlId="imageUrlForm">
-              <img src={props.imgSrcInfo ? props.imgSrcInfo : MillionMaticPageSymbolSold} alt={props.altTextInfo} width="100%" style={imageStyle} />
-              <Form.Label>Image Url</Form.Label> 
-              <Form.Control type="text" placeholder={props.imgSrcInfo ? "Old: " + props.imgSrcInfo : "Insert your NFT image url"} name='imageUrlControl' onChange={handleChange}/>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="altTextForm">
-              <Form.Label>Alt Text</Form.Label>
-              <Form.Control type="text" placeholder={props.altTextInfo ? "Old: " + props.altTextInfo : "Insert your NFT alt text"} name='altTextControl' onChange={handleChange}/>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="webURLForm">
-              <Form.Label>Web Url</Form.Label>
-              <Form.Control type="text" placeholder={props.webUrlInfo ? "Old: " + props.webUrlInfo : "Insert your NFT web url"} name='webURLControl' onChange={handleChange}/>
-            </Form.Group>
-          </Form>
+			<Modal.Body>
+			<Form>
+				<Form.Group className="mb-3" controlId="imageUrlForm">
+				<img src={props.imgSrcInfo ? props.imgSrcInfo : MillionMaticPageSymbolSold} alt={props.altTextInfo} width="100%" style={imageStyle} />
+				<Form.Label>Image Url</Form.Label> 
+				<Form.Control type="text" placeholder={props.imgSrcInfo ? "Old: " + props.imgSrcInfo : "Insert your NFT image url"} name='imageUrlControl' onChange={handleChange}/>
+				</Form.Group>
+				<Form.Group className="mb-3" controlId="altTextForm">
+				<Form.Label>Alt Text</Form.Label>
+				<Form.Control type="text" placeholder={props.altTextInfo ? "Old: " + props.altTextInfo : "Insert your NFT alt text"} name='altTextControl' onChange={handleChange}/>
+				</Form.Group>
+				<Form.Group className="mb-3" controlId="webURLForm">
+				<Form.Label>Web Url</Form.Label>
+				<Form.Control type="text" placeholder={props.webUrlInfo ? "Old: " + props.webUrlInfo : "Insert your NFT web url"} name='webURLControl' onChange={handleChange}/>
+				</Form.Group>
+			</Form>
 
-        </Modal.Body>
+			</Modal.Body>
 
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={async () => {
-            console.log('Invio i dati a IPFS...'); 
-            setContentIdentificator(await sendDataToIPFS());
-            console.log('Dati salvati correttamente!'); 
-            console.log('Invio i dati alla blockchain...'); 
-            await writeToBlockchain();
-            console.log('Dati salvati correttamente!'); 
-          }}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </div>
-  );
+			<Modal.Footer>
+			<Button variant="secondary" onClick={handleClose}>
+				Close
+			</Button>
+			<Button variant="primary" onClick={async () => {
+				//console.log('Invio i dati a IPFS...'); 
+				sendDataToIPFS();
+			}}>
+				Save Changes
+			</Button>
+			</Modal.Footer>
+		</Modal>
+		</div>
+	);
 }
