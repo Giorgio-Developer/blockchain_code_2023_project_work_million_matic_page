cd16e0663bd22766289d51bff9571bd3aafa711a
RiccardoMolinari95 <riccardo95molinari@gmail.com>
2023-05-27 11:00:57 +0200

    Save changes (IPFS + setTokenUri)  now works correctly

--- mmp-fe/src/modals/editModal.tsx
+++ mmp-fe/src/modals/editModal.tsx
@@ -4,92 +4,77 @@ import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
 import { CSSProperties, useEffect, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useContractWrite, usePrepareContractWrite } from 'wagmi';
+import { useContract, useSigner } from 'wagmi';
 import { contractAbi } from "../constant/contract-abi";
 import MillionMaticPageSymbolSold from "../images/MillionMaticPageSymbolSold.png";
+
 
 var axios = require('axios');
 
 const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS as `0x${string}`;
-
 const PINATA_APIKEY = process.env.REACT_APP_PINATA_APIKEY;
 const PINATA_SECRET = process.env.REACT_APP_PINATA_SECRET;
 const abi = contractAbi;
 
 export function EditModal(props: any) {
 
-interface TypeDataToSend {
-	name:string;
-	image: string;
-	description: string;
-	external_url: string;
-	tokenId: number;
-}
+	interface TypeDataToSend {
+		name:string;
+		image: string;
+		description: string;
+		external_url: string;
+		tokenId: number;
+	}
 
-const imageStyle = {
-	width: '100%',
-	padding: '20px',
-	margin: '0px',
-	border: '1px solid black',
-};
+	const imageStyle = {
+		width: '100%',
+		padding: '20px',
+		margin: '0px',
+		border: '1px solid black',
+	};
 
-  const ownerAddressStyle: CSSProperties = {
+	const ownerAddressStyle: CSSProperties = {
 		textAlign: 'left',
 		fontSize: '1.05em',
 		marginLeft: '3%',
 	};
 
-const [name, setName] = useState('');
-const [nftImage, setNftImage] = useState('');
-const [description, setDescription] = useState('');
-const [externalURL, setExternalURL] = useState('');
-const [tokenId, setTokenId] = useState("");
-const [contentIdentificator, setContentIdentificator] = useState('');
-const handleClose = () => {props.clickCloseButton()} ; // () => setShow(false);
+	const [name, setName] = useState('');
+	const [nftImage, setNftImage] = useState('');
+	const [description, setDescription] = useState('');
+	const [externalURL, setExternalURL] = useState('');
+	const [tokenId, setTokenId] = useState(0);
+	const handleClose = () => {props.clickCloseButton()} ; // () => setShow(false);
 
-const nftOpenSeaLink = process.env.REACT_APP_OPENSEA_BASE_URL + contractAddress + "/" + props.tokenId;
-
+	const nftOpenSeaLink = process.env.REACT_APP_OPENSEA_BASE_URL + contractAddress + "/" + props.tokenId;
 
 
-let txExecuting = false;
 
-const { config } = usePrepareContractWrite({
-	address: contractAddress,
-	abi: abi,
-	functionName: "setTokenURI",
-	args: [tokenId, contentIdentificator]
-});
+	let txExecuting = false;
 
-const { data, isLoading, isSuccess, isError, write } = useContractWrite(config);
+	const contract = useContract({
+		abi: abi,
+		address: contractAddress
+	})
+
+
+	const { data:signer } = useSigner();
 
 	useEffect(() => {
 		if (props.show === true) {
-		setTokenId(props.tokenId);
-		console.log(props.tokenId);
+			setTokenId(props.tokenId);
+			console.log(props.tokenId);
 		}
 	}, [props.show]);
 
-	useEffect(() => {
-		if (contentIdentificator !== undefined && contentIdentificator !== '' && !txExecuting) {
-		//writeToBlockchain();
-		console.log('writing on Blockchain');
-		if (!write) return;
-		console.log(config)
-		write(); // write to blockchain
 
-		}
-	}, [contentIdentificator]);
-
-	const sendDataToIPFS = () => {
+	const sendDataToIPFS = async () => {
 
 		console.log('sendDataToIPFS Started...');
 		//setLoading(true);
-		props.setLoadingSpinner(true);
+		//props.setLoadingSpinner(true);
 
-
-  //const { data, isLoading, isSuccess, isError,  write } = useContractWrite(config);
-
-		const dataToSend:TypeDataToSend = {
+		const dataToSend: TypeDataToSend = {
 			name: name,
 			image: nftImage,
 			description: description,
@@ -104,8 +89,8 @@ const { data, isLoading, isSuccess, isError, write } = useContractWrite(config);
 			pinataMetadata: {
 				name: "NFTData",
 				keyvalues: {
-				customKey: "customValue",
-				customKey2: "customValue2",
+					customKey: "customValue",
+					customKey2: "customValue2",
 				},
 			},
 			pinataContent: dataToSend,
@@ -122,139 +107,135 @@ const { data, isLoading, isSuccess, isError, write } = useContractWrite(config);
 			data: dataIPFS,
 		};
 
-		const res = axios(configIPFS)
-			.then(function (response: any) {
-				// console.log('ipfsHash: ', response.data.IpfsHash);
-				setContentIdentificator(response.data.IpfsHash);
-				console.log('contentIdentificator: ', contentIdentificator);
-			})
-			.catch(function (error: any) {
-				// handle error
-				console.log(error);
-			});
+		const res = await axios(configIPFS);
+		let cid = res.data.IpfsHash;
+		console.log(cid);
 
-			
+		if(res && signer){
+			contract?.connect(signer).setTokenURI(tokenId,cid)
+		}
+
 	};
 
 
 
-  function handleChange(event:any) {
-	let targetId = event.currentTarget.id;
+	function handleChange(event:any) {
+		let targetId = event.currentTarget.id;
 
-	  switch(targetId) {
-		  case 'nameForm':
-			  setName(event.target.value);
-			  break;
-		  case 'nftImageForm':
-			  setNftImage(event.target.value);
-			  break;
-		  case 'descriptionForm':
-			  setDescription(event.target.value);
-			  break;
-		  case 'externalURLForm':
-			  setExternalURL(event.target.value);
-			  break;
-	  }
-
-  }
-
-  //const [loading, setLoading] = useState(false);
-  
-  const polygon_base_uri = "https://mumbai.polygonscan.com/tx/";
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const handleTransactionStart = () => {
-    txExecuting = true;
-		console.log('Salvataggio dati su Blockchain in corso...');
-    //setLoading(true);
-    props.setLoadingSpinner(true);
-  }
-  const handleTransactionSuccess = () => {
-      txExecuting = false;
-      console.log('Dati salvati correttamente su Blockchain');  
-      //setLoading(false);
-      props.setLoadingSpinner(false);
-      handleClose();
-  }
-  const handleTransactionError = () => {
-      //setLoading(false); 			// Imposta lo stato di loading a false una volta completata o fallita la transazione
-      props.setLoadingSpinner(false);
-  }
-
-  useEffect(() => {
-		if (isLoading) {
-			handleTransactionStart();
+		switch(targetId) {
+			case 'nameForm':
+				setName(event.target.value);
+				break;
+			case 'nftImageForm':
+				setNftImage(event.target.value);
+				break;
+			case 'descriptionForm':
+				setDescription(event.target.value);
+				break;
+			case 'externalURLForm':
+				setExternalURL(event.target.value);
+				break;
 		}
-	}, [handleTransactionStart, isLoading]);
 
-  useEffect(() => {
-		if (isSuccess && data ) {
-			handleTransactionSuccess();
-		}
-	}, [handleTransactionSuccess, isSuccess]);
+	}
 
-	useEffect(() => {
-		if (isError) {
-			handleTransactionError();
-		}
-	}, [handleTransactionError, isError]);
+	//const [loading, setLoading] = useState(false);
 
-  return (
+	const polygon_base_uri = "https://mumbai.polygonscan.com/tx/";
+
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	/*  const handleTransactionStart = () => {
+        txExecuting = true;
+            console.log('Salvataggio dati su Blockchain in corso...');
+        //setLoading(true);
+        props.setLoadingSpinner(true);
+      }
+      const handleTransactionSuccess = () => {
+          txExecuting = false;
+          console.log('Dati salvati correttamente su Blockchain');
+          //setLoading(false);
+          props.setLoadingSpinner(false);
+          handleClose();
+      }
+      const handleTransactionError = () => {
+          //setLoading(false); 			// Imposta lo stato di loading a false una volta completata o fallita la transazione
+          props.setLoadingSpinner(false);
+      }
+
+      useEffect(() => {
+            if (isLoading) {
+                handleTransactionStart();
+            }
+        }, [handleTransactionStart, isLoading]);
+
+      useEffect(() => {
+            if (isSuccess && data ) {
+                handleTransactionSuccess();
+            }
+        }, [handleTransactionSuccess, isSuccess]);
+
+        useEffect(() => {
+            if (isError) {
+                handleTransactionError();
+            }
+        }, [handleTransactionError, isError]);*/
+
+	return (
 		<div
-		className="modal show"
-		style={{ display: 'block', position: 'initial' }}
+			className="modal show"
+			style={{ display: 'block', position: 'initial' }}
 		>
 
-		<Modal show={props.show} onHide={handleClose}>
-			<Modal.Header closeButton>
-			<Modal.Title>Edit Modal - NFT: #{props.tokenId}</Modal.Title>
-			</Modal.Header>
-			<Modal.Body>
-				<Form>
-					<img src={props.nftImageInfo ? props.nftImageInfo : MillionMaticPageSymbolSold} alt={props.descriptionInfo} width="100%" style={imageStyle} />
-					<Form.Group className="mb-3" controlId="nameForm">
-						<Form.Label>Name</Form.Label>
-						<Form.Control type="text" placeholder={props.nameInfo ? "Old: " + props.nameInfo : "Insert your NFT name"} name='nameControl' onChange={handleChange}/>
-					</Form.Group>
-					<Form.Group className="mb-3" controlId="nftImageForm">
-						<Form.Label>Image Url</Form.Label>
-						<Form.Control type="text" placeholder={props.nftImageInfo ? "Old: " + props.nftImageInfo : "Insert your NFT image url"} name='imageUrlControl' onChange={handleChange}/>
-					</Form.Group>
-					<Form.Group className="mb-3" controlId="descriptionForm">
-						<Form.Label>Description</Form.Label>
-						<Form.Control type="text" placeholder={props.descriptionInfo ? "Old: " + props.descriptionInfo : "Insert your NFT description"} name='descriptionControl' onChange={handleChange}/>
-					</Form.Group>
-					<Form.Group className="mb-3" controlId="externalURLForm">
-						<Form.Label>Web Url</Form.Label>
-						<Form.Control type="text" placeholder={props.externalURLInfo ? "Old: " + props.externalURLInfo : "Insert your NFT web url"} name='externalURLControl' onChange={handleChange}/>
-					</Form.Group>
-				</Form>
+			<Modal show={props.show} onHide={handleClose}>
+				<Modal.Header closeButton>
+					<Modal.Title>Edit Modal - NFT: #{props.tokenId}</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<Form>
+						<img src={props.nftImageInfo ? props.nftImageInfo : MillionMaticPageSymbolSold} alt={props.descriptionInfo} width="100%" style={imageStyle} />
+						<Form.Group className="mb-3" controlId="nameForm">
+							<Form.Label>Name</Form.Label>
+							<Form.Control type="text" placeholder={props.nameInfo ? "Old: " + props.nameInfo : "Insert your NFT name"} name='nameControl' onChange={handleChange}/>
+						</Form.Group>
+						<Form.Group className="mb-3" controlId="nftImageForm">
+							<Form.Label>Image Url</Form.Label>
+							<Form.Control type="text" placeholder={props.nftImageInfo ? "Old: " + props.nftImageInfo : "Insert your NFT image url"} name='imageUrlControl' onChange={handleChange}/>
+						</Form.Group>
+						<Form.Group className="mb-3" controlId="descriptionForm">
+							<Form.Label>Description</Form.Label>
+							<Form.Control type="text" placeholder={props.descriptionInfo ? "Old: " + props.descriptionInfo : "Insert your NFT description"} name='descriptionControl' onChange={handleChange}/>
+						</Form.Group>
+						<Form.Group className="mb-3" controlId="externalURLForm">
+							<Form.Label>Web Url</Form.Label>
+							<Form.Control type="text" placeholder={props.externalURLInfo ? "Old: " + props.externalURLInfo : "Insert your NFT web url"} name='externalURLControl' onChange={handleChange}/>
+						</Form.Group>
+					</Form>
 
 
-				<div>
+					{/*				<div>
 					{isLoading && <div>Loading...</div>}
-					{isError && <div id="messageContainer"><br/>Per aggiornare i dati è necessario confermare la transazione tramite il proprio wallet<br/><br/></div>}
-				</div>
-				<div id="btnsContainer">
-					<Button variant="primary" onClick={sendDataToIPFS}>
-						Save Changes
+					{isError && <div id="messageContainer"><br/>Per aggiornare i dati è necessario confermare la transazione tramite il proprio wallet<br/><br/></div>}*/}
+					{/*			</div>*/}
+					<div id="btnsContainer">
+						<Button variant="primary" onClick={sendDataToIPFS}>
+							Save Changes
+						</Button>
+						&nbsp;
+						<a href={nftOpenSeaLink} target="_blank" rel="noreferrer">
+							<Button>Look on OpenSea</Button>
+						</a>
+					</div>
+
+				</Modal.Body>
+
+				<Modal.Footer>
+					<Button variant="secondary" onClick={handleClose}>
+						Close
 					</Button>
-					&nbsp;
-					<a href={nftOpenSeaLink} target="_blank" rel="noreferrer">
-						<Button>Look on OpenSea</Button>
-					</a>
-				</div>
-
-			</Modal.Body>
-
-			<Modal.Footer>
-			<Button variant="secondary" onClick={handleClose}>
-				Close
-			</Button>
 
 
-			</Modal.Footer>
-		</Modal>
+				</Modal.Footer>
+			</Modal>
 		</div>
 	);
 }
